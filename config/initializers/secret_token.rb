ba113297915cf5aa369064922e842dea783cf7c3
Akshay Gupta <akshay.aries11@gmail.com>
2012-08-03 18:18:18 +0530

    initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DakiaWeb::Application.config.secret_token = '6ac467fe9dfc0f42a031a8739e0443b6b349a0fd322c5a7f4a98a747f807425c5cfd5595fac2d1b9e9f4d45965d731b613c34c862763efefd44a91b70e54ad08'
