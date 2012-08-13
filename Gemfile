7a1c19f65e0f97f06dc11c3f8a4e159fd0624578
Akshay Gupta <akshay.aries11@gmail.com>
2012-08-14 01:25:48 +0530

    assets sync gem added and configuration added

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem 'aws-sdk'
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-
+  gem 'asset_sync'
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   gem 'therubyracer', :platforms => :ruby
   gem 'uglifier', '>= 1.0.3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ DakiaWeb::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -45,6 +45,7 @@ DakiaWeb::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://files.edakia.in" #"https://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
