57cb590acdc50d465581e049652a1fba6c0630d3
Akshay Gupta <akshay.aries11@gmail.com>
2012-08-20 23:32:56 +0530

    s3 configuration

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,5 +1,5 @@
 # initializers/s3.rb
-if Rails.env == "production"
+if Rails.env != "development"
   # set credentials from ENV hash
   S3_CREDENTIALS = { :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], :bucket => ENV['S3_BUCKET_NAME']}
 else
