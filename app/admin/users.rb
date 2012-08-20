f47649fb72360bd7ec379a05f05c7fb87f51cc47
Akshay Gupta <akshay.aries11@gmail.com>
2012-08-20 14:14:24 +0530

    Admin user interface updated

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,44 @@
 ActiveAdmin.register User do
+  index do
+    column :first_name
+    column :last_name
+    column :email
+    column :mobile
+    column :balance
+    column :last_sign_in_at
+    column :sign_in_count
+    default_actions
+  end
+  
+  
+  form do |f|
+    f.inputs do
+      f.input :first_name
+      f.input :last_name
+      f.input :mobile
+      f.input :password
+      f.input :referee 
+      f.input :password_confirmation
+    end
+
+    f.buttons
+  end
+  
+  show do |user|
+    attributes_table do
+      row :id
+      row :email
+      row :mobile
+      row :first_name
+      row :last_name
+      row :balance
+      row :referee
+      row :sign_in_count
+      row :last_sign_in_at
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
   
 end
