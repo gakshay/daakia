74cf4db5363d81145ed3e75503fcd44ee14c28d9
Akshay Gupta <akshay.aries11@gmail.com>
2012-08-20 14:21:02 +0530

    admin transactions/mail interface updated

--- app/admin/transactions.rb
+++ app/admin/transactions.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Transaction do
+  menu :label => "Mails"
+  actions  :index, :new, :create, :show, :destroy
+  
+  index do
+    column :sender_mobile
+    column :receiver_mobile
+    column :receiver_email
+    column "D/L Count", :download_count
+    column :read
+    column :created_at
+    default_actions
+  end
   
 end
