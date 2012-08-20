c8fba3df034297be76b4794f4619a356aea435ce
Akshay Gupta <akshay.aries11@gmail.com>
2012-08-20 14:14:46 +0530

    Admin documents/files interface updated

--- app/admin/documents.rb
+++ app/admin/documents.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Document do
+  menu :label => "Files"
+  actions  :index, :new, :create, :show, :destroy
   
+  index do
+    column "File Name", :doc_file_name
+    column "File Type", :doc_content_type
+    column "File Size", :doc_file_size
+    column :direct_url
+    column :pages
+    column :doc_updated_at
+    default_actions
+  end
 end
