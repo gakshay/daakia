ca5cbbf8c9fbd1d7f5ec17dc4679cc3bc262c58b
Akshay Gupta <akshay.aries11@gmail.com>
2012-09-02 10:46:18 +0530

    support for xls, ppt, xlsx, pptx, html, plain files

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < ActiveRecord::Base
   attr_accessible :doc, :user_id, :pages
   has_attached_file :doc, :use_timestamp => true, :storage => :s3, :s3_credentials => S3_CREDENTIALS
   validates_attachment_presence :doc
-  validates_attachment_content_type :doc, :content_type => [ 'image/jpg', 'image/jpeg', 'image/png', 'image/gif', 'image/bmp', 'application/msword', 'application/pdf', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' ], :message => "File format not supported"
+  validates_attachment_content_type :doc, :content_type => [ 'image/jpg', 'image/jpeg', 'image/png', 'image/gif', 'image/bmp', 'text/html', 'text/plain', 'application/msword', 'application/pdf', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ], :message => "File format not supported"
   validates_attachment_size :doc, :less_than => 5.megabyte, :message => "File must be less than 5MB", :if => Proc.new { |imports| !imports.doc_file_name.blank? }
   validates_presence_of :user_id
   
