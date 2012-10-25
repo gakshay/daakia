3fb9b36edd9c3964dc547037e7e8e3fbe6d95a66
Akshay Gupta <akshay.aries11@gmail.com>
2012-10-25 23:51:22 +0530

    serial number must be 10 character long min

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -6,7 +6,7 @@ class Api::TransactionsController < TransactionsController
   protected
   
   def parse_serial_number
-    if params[:serial_number].blank? || params[:serial_number].length <= 16
+    if params[:serial_number].blank? || params[:serial_number].length <= 9
       respond_with :status => :unprocessable_entity 
     end
   end
