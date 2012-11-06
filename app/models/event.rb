af72ee5b1033c5b2cc7e264eec3fb63ab4aca021
Akshay Gupta <akshay.aries11@gmail.com>
2012-11-06 23:24:36 +0530

    resolved event active=1 issue

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   attr_accessible :action, :user, :cost, :machine_id
     
-  belongs_to :transaction, :conditions => "active=1"
+  belongs_to :transaction
   belongs_to :machine, :counter_cache => true
   
   validates_presence_of :user
